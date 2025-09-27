68332ddfaceb7e13c58ded056fc0e7edfddfcab7
Ricardo Abreu <ricab@ricabhome.org>
2025-09-27 02:22:54 +0100

    [cli] Add basic CLI with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    author = "Ricardo Abreu, ricab@ricabhome.org",
+    about = "Count reviews on a GitHub repository"
+)]
+struct Args {
+    /// GitHub repository in format owner/repo
+    repo: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Repository: {}", args.repo);
 }
