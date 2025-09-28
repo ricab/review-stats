faf3e0ab73f191bb561dff7a5a793748b75559a8
Ricardo Abreu <ricab@ricabhome.org>
2025-09-28 03:03:04 +0100

    [timeframe] Add constructor and accessors

--- src/timeframe.rs
+++ src/timeframe.rs
@@ -2,6 +2,31 @@ use chrono::{DateTime, Utc};
 
 #[derive(Debug, Clone)]
 pub struct Timeframe {
-    pub start: DateTime<Utc>,
-    pub end: DateTime<Utc>,
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
+}
+
+impl Timeframe {
+    pub fn new(start: DateTime<Utc>, end: DateTime<Utc>) -> Self {
+        Self { start, end }
+    }
+
+    pub fn start(&self) -> DateTime<Utc> { self.start }
+    pub fn end(&self) -> DateTime<Utc> { self.end }
+}
+
+#[cfg(test)]
+mod test {
+    use chrono::{TimeZone, Utc};
+    use crate::timeframe::Timeframe;
+
+    #[test]
+    fn accessors_behave() {
+        let begin = Utc.timestamp_opt(1234567890, 321).unwrap();
+        let finish = Utc.timestamp_opt(9876543210, 123).unwrap();
+        let uut = Timeframe::new(begin, finish);
+
+        assert_eq!(uut.start(), begin);
+        assert_eq!(uut.end(), finish);
+    }
 }
