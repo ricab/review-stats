10895b024d0a4c8491e10f3aa7fe2224dffc4997
Ricardo Abreu <ricab@ricabhome.org>
2025-10-06 02:08:46 +0100

    [timeframe] Derive Clone

--- src/timeframe.rs
+++ src/timeframe.rs
@@ -7,7 +7,7 @@ use std::str::FromStr;
 use std::sync::LazyLock;
 
 /// An absolute window of forward earth time with inclusive boundaries
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Timeframe {
     begin: DateTime<Utc>,
     end: DateTime<Utc>,
