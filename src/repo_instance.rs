b4999e90e472acc23415b822ed6e593d46766584
Ricardo Abreu <ricab@ricabhome.org>
2025-10-19 03:30:18 +0100

    [repo] Add initial RepoInstance implementation

--- src/repo_instance.rs
+++ src/repo_instance.rs
@@ -1,1 +1,20 @@
-pub struct RepoInstance;
+use crate::{BoxedError, Result};
+
+pub struct RepoInstance {
+    owner: String,
+    repo: String,
+}
+
+// Custom public interface
+impl RepoInstance {
+    pub fn new(owner: String, repo: String) -> Self {
+        Self { owner, repo }
+    }
+}
+
+impl std::str::FromStr for RepoInstance {
+    type Err = BoxedError;
+    fn from_str(s: &str) -> Result<Self> {
+        todo!()
+    }
+}
